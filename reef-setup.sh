3cbb6b723981e9137722d93b9463a23991d40b9c
Dwigt007 <37881405+Dwigt007@users.noreply.github.com>
2018-05-22 18:13:10 -0500

    Update reef-setup.sh

--- reef-setup.sh
+++ reef-setup.sh
@@ -10,7 +10,7 @@ YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
 #Reef TCP port
-PORT=9058
+PORT=12058
 
 #Clear keyboard input buffer
 function clear_stdin { while read -r -t 0; do read -r; done; }
