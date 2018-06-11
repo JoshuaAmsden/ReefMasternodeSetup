34ad3776043158adf02b08f2c30445dd758dc07e
JoshuaAmsden <joshamsden@msn.com>
2018-06-11 04:53:39 -0600

    Update nodemon.sh

--- nodemon.sh
+++ nodemon.sh
@@ -10,15 +10,6 @@ datadir="/$USER/.reefcore$2"   # Default datadir is /root/.reefcore
 dpkg -s jq 2>/dev/null >/dev/null || sudo apt-get -y install jq
 
 #It is a one-liner script for now
-watch -ptn $dly "echo '===========================================================================
-Outbound connections to other Reef nodes [reef datadir: $datadir]
-===========================================================================
-Node IP               Ping    Rx/Tx     Since  Hdrs   Height  Time   Ban
-Address               (ms)   (KBytes)   Block  Syncd  Blocks  (min)  Score
-==========================================================================='
-reef-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor) ,\
-\(.bytesrecv/1024|floor)/\(.bytessent/1024|floor),\(.startingheight) ,\(.synced_headers) ,\(.synced_blocks)  ,\
-\((now-.conntime)/60|floor) ,\(.banscore)\"' | column -t -s ',' && 
 echo '==========================================================================='
 uptime
 echo '==========================================================================='
@@ -28,6 +19,15 @@ echo 'Sync Status: \n# reef-cli mnsync status' &&  reef-cli -datadir=$datadir mn
 echo '==========================================================================='
 echo 'Masternode Information: \n# reef-cli getinfo' && reef-cli -datadir=$datadir getinfo
 echo '==========================================================================='
+watch -ptn $dly "echo '===========================================================================
+Outbound connections to other Reef nodes [reef datadir: $datadir]
+===========================================================================
+Node IP               Ping    Rx/Tx     Since  Hdrs   Height  Time   Ban
+Address               (ms)   (KBytes)   Block  Syncd  Blocks  (min)  Score
+==========================================================================='
+reef-cli -datadir=$datadir getpeerinfo | jq -r '.[] | select(.inbound==false) | \"\(.addr),\(.pingtime*1000|floor) ,\
+\(.bytesrecv/1024|floor)/\(.bytessent/1024|floor),\(.startingheight) ,\(.synced_headers) ,\(.synced_blocks)  ,\
+\((now-.conntime)/60|floor) ,\(.banscore)\"' | column -t -s ',' && 
 echo 'Usage: reefmon.sh [refresh delay] [datadir index]'
 echo 'Example: reefmon.sh 10 22 will run every 10 seconds and query reefd in /$USER/.reefcore22'
 echo '\n\nPress Ctrl-C to Exit...'"
